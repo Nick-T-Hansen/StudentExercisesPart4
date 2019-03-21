a7c216a80906ddd3b09b8e335af46aa6fbe17feb
Nick Hansen <nick.hansen13@gmail.com>
2019-03-21 10:17:10 -0500

    Add project files.

---
+++ StudentExercisesPart4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentExercisesPart4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
