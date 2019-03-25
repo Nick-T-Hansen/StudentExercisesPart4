eff06540cce08dc178b1d7e13213bc3971fce2a8
Nick Hansen <nick.hansen13@gmail.com>
2019-03-25 08:42:58 -0500

    first commit

---
+++ StudentExercisesPart4/Data/Repository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace StudentExercisesPart4.Data
+{
+    public class Repository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                string connectionString = "Server=DESKTOP-T1CK9M7\\SQLEXPRESS;Database=StudentExercises;Trusted_Connection=True;";
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public List<Exercise> GetAllExercises()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, [ExerciseName], [ExerciseLanguage] from Exercise;";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Exercise> exercises = new List<Exercise>();
+
+                    while (reader.Read())
+                    {
+                        Exercise exercise = new Exercise
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                            ExerciseLanguage = reader.GetString(reader.GetOrdinal("ExerciseLanguage"))
+                        };
+
+                        exercises.Add(exercise);
+                    }
+
+                    reader.Close();
+                    return exercises;
+                }
+            }
+        }
+        public void AddExercise(Exercise exercise)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $@"INSERT INTO Exercise (ExerciseName, ExerciseLanguage)
+                                           VALUES ('{exercise.ExerciseName}', '{exercise.ExerciseLanguage}')";
+                    cmd.ExecuteNonQuery();
+    
+                }
+            }
+        }
+        /*
+        public List<Student> GetAllStudents()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"select s.id as StudentId,
+                                        s.FirstName,
+                                        s.LastName,
+                                        s.SlackHandle,
+                                        s.CohortId,
+                                        c.[Name] as CohortName,
+                                        e.id as ExerciseId,
+                                        e.[name] as ExerciseName,
+                                        e.[Language]
+                                    from student s
+                                        left join Cohort c on s.CohortId = c.id
+                                        left join StudentExercise se on s.id = se.studentid
+                                        left join Exercise e on se.exerciseid = e.id;";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                   
+                    Dictionary<int, Student> students = new Dictionary<int, Student>();
+                    while (reader.Read())
+                    {
+                        int studentId = reader.GetInt32(reader.GetOrdinal("StudentId"));
+                        if (!students.ContainsKey(studentId))
+                        {
+                            Student newStudent = new Student
+                            {
+                                Id = studentId,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                Slack = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                                CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
+
+                            };
+
+                            students.Add(newStudent);
+                            reader.Close();
+                            return students;
+                        }
+                        /*
+                        if (!reader.IsDBNull(reader.GetOrdinal("ExerciseId")))
+                        {
+                            Student currentStudent = students[studentId];
+                            currentStudent.Exercise.Add(
+                                new Exercise
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ExerciseId")),
+                                    ExerciseLanguage = reader.GetString(reader.GetOrdinal("Language")),
+                                    ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                                }
+                            );
+                        }
+                    }
+                    reader.Close();
+
+                    return students.Values.ToList();
+                    */
+               // }
+          //  }
+        //
+    }
+}       
--- StudentExercisesPart4/Program.cs
+++ StudentExercisesPart4/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudentExercisesPart4.Data;
+using System;
+using System.Collections.Generic;
 
 namespace StudentExercisesPart4
 {
@@ -6,7 +8,40 @@ namespace StudentExercisesPart4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Query the database for all the Exercises.
+            Find all the exercises in the database where the language is JavaScript.
+            Insert a new exercise into the database.
+            Find all instructors in the database.Include each instructor's cohort.
+            Insert a new instructor into the database.Assign the instructor to an existing cohort.
+            Assign an existing exercise to an existing student.
+            */
+
+            Repository repository = new Repository();
+
+            //GET all exercises
+
+            List<Exercise> AllExercises = repository.GetAllExercises();
+
+            foreach(Exercise e in AllExercises)
+            {
+                Console.WriteLine($"{e.ExerciseName}");
+            }
+
+            //Insert a new exercise in the database
+
+            Exercise Stocks = new Exercise {
+                ExerciseName = "Stocks",
+                ExerciseLanguage = "C#" };
+
+            repository.AddExercise(Stocks);
+            Console.WriteLine($"{Stocks.ExerciseLanguage} was added to the database");
+
+            foreach (Exercise e in AllExercises)
+            {
+                Console.WriteLine($"{e.ExerciseName}");
+            }
+            Console.ReadKey();
         }
     }
 }
